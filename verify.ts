a8173fe67cca9948a80c24488647d9f5ffee221d
argeric <argeric@qq.com>
2025-07-03 16:57:36 +0800

    Using absolute path of build-info file.

--- verify.ts
+++ verify.ts
@@ -4,7 +4,7 @@ import {sendSingleFileVerifyRequest} from "./verification/single-file";
 // ******************************** code-format: Standard json input ********************************
 // evm space
 // build-info: ./verification/0d76fa1e0c3cdbb18d383e740a03426f
-// open-api: https://evmapi-testnet-stage.confluxscan.org/api
+// open-api: https://evmapi-testnet.confluxscan.org/api
 // contracts/reward/ChunkLinearReward.sol:ChunkLinearReward:0x15EA79340DE75d7a7Dc87304B8a04D95f78eD506
 // contracts/market/FixedPrice.sol:FixedPrice:0x0C0Af94B384ba3656755cd877a28F5a3d406Fb06
 // contracts/dataFlow/FixedPriceFlow.sol:FixedPriceFlow:0xf69051CF06199b856e5E15e87021Dcb5aC12A899
@@ -18,7 +18,7 @@ sendJsonInputVerifyRequest(
 "contracts/miner/Mine.sol", // Fill in your contract path. Usually could be seen in build-info.output.sources[contract-path]
 "PoraMine", // Fill in your contract name. Usually could be seen in build-info.output.sources[contract-path].[contract-name]
 '0xf1084A4E9d232D83f33daC3310913A00Ee21B5D4', // Fill in your contract address
-'https://evmapi-testnet-stage.confluxscan.org/api', // Fill in the URL of verify-sourcecode OpenAPI
+'https://evmapi-testnet.confluxscan.org/api', // Fill in the URL of verify-sourcecode OpenAPI
 ).catch(err => {
   console.error('verification error', err)
 })
@@ -26,7 +26,7 @@ sendJsonInputVerifyRequest(
 // ************************************ code-format: Single file ************************************
 // evm space
 // build-info: ./verification/02fe6bf3803d3515766215d24eb50335
-// open-api: https://evmapi-testnet-stage.confluxscan.org/api
+// open-api: https://evmapi-testnet.confluxscan.org/api
 // contracts/PRCC_Vesting.sol:PRCC_Vesting:0xb856d19c518d74ee36964179d82a1a6218d26012
 // core space
 // build-info: ./verification/02fe6bf3803d3515766215d24eb50335
@@ -37,7 +37,7 @@ sendSingleFileVerifyRequest(
     "contracts/PRCC_Vesting.sol", // Fill in your contract path. Usually could be seen in build-info.output.sources[contract-path]
     "PRCC_Vesting", // Fill in your contract name. Usually could be seen in build-info.output.sources[contract-path].[contract-name]
     '0xb856d19c518d74ee36964179d82a1a6218d26012', // Fill in your contract address.
-    'https://evmapi-testnet-stage.confluxscan.org/api', // Fill in the URL of verify-sourcecode OpenAPI
+    'https://evmapi-testnet.confluxscan.org/api', // Fill in the URL of verify-sourcecode OpenAPI
 ).catch(err => {
   console.error('verification error', err)
 })
